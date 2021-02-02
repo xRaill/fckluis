6ae3ed693fd638268ed494189db9a3cf974e1019
Railly Koeiman <raillykoeiman@gmail.com>
2021-02-02 14:35:53 +0100

    Switched to typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;

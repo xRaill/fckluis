10b6b3e3be61f21ae3dd359a55c47059055a382b
Railly Koeiman <raillykoeiman@gmail.com>
2021-02-02 16:28:11 +0100

    Added styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { createGlobalStyle, ThemeProvider, DefaultTheme } from 'styled-components';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+const theme: DefaultTheme = {
+  colors: {
+    background: '#fff'
+  }
+}
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: ${({ theme }) => theme.colors.background}
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-border-box;
+  }
+`;
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default MyApp;
---
+++ styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+
+// Extend styled-components to add theme types
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      background: string;
+    };
+  }
+}

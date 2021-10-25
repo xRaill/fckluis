44179c3c7d38e99fac2254a43028eec68870c124
Railly Koeiman <raillykoeiman@gmail.com>
2021-10-25 18:07:11 +0200

    Fixed some env vars

--- utils/nodemailer.ts
+++ utils/nodemailer.ts
@@ -1,12 +1,12 @@
 import { createTransport } from 'nodemailer';
-const { PRODUCTION, MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASS } = process.env;
+const { NODE_ENV, MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASS } = process.env;
 
 const transporter = createTransport({
   host: MAIL_HOST,
   port: MAIL_PORT as unknown as number,
-  secure: !!PRODUCTION,
+  secure: NODE_ENV === 'production',
   tls: {
-    rejectUnauthorized: !!PRODUCTION,
+    rejectUnauthorized: NODE_ENV === 'production',
   },
   auth: {
     user: MAIL_USER,
